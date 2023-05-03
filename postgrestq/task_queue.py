10d111874ce1b4d85fa566e698e59cfed0fec24d
Harika Duyu <harika.duyu@gmail.com>
2023-05-03 10:19:19 +0200

    💅 some refactoring

--- postgrestq/task_queue.py
+++ postgrestq/task_queue.py
@@ -1,7 +1,7 @@
 import json
 import logging
-import time
-from uuid import uuid4
+# import time
+from uuid import uuid4, UUID
 
 import psycopg as pg
 
@@ -10,7 +10,14 @@ logger = logging.getLogger(__name__)
 
 
 class TaskQueue:
-    def __init__(self, dsn, queue_name, table_name: str = 'task_queue', reset=False, ttl_zero_callback=None):
+    def __init__(
+        self,
+        dsn: str,
+        queue_name: str,
+        table_name: str = 'task_queue',
+        reset: bool = False,
+        ttl_zero_callback=None
+    ):
         """Initialize the task queue.
 
         Note: a task has to be at any given time either in the task
@@ -55,7 +62,7 @@ class TaskQueue:
 
     def _create_queue_table(self):
         """
-        Creates a task_queue table        
+        Creates a task_queue table
         """
         # TODO: check if the table already exist
         # whether it has the same schema
@@ -68,7 +75,7 @@ class TaskQueue:
                         ttl INT NOT NULL,
                         created_at TIMESTAMP NOT NULL DEFAULT NOW(),
                         processing BOOLEAN NOT NULL DEFAULT false,
-                        lease_timeout FLOAT, 
+                        lease_timeout FLOAT,
                         deadline TIMESTAMP,
                         completed_at TIMESTAMP
                     )
@@ -80,12 +87,13 @@ class TaskQueue:
         """
         with self.conn.cursor() as cursor:
             cursor.execute(f"""
-                SELECT count(1) as count 
+                SELECT count(1) as count
                 FROM {self._table_name}
                 WHERE queue_name = '{self._queue_name}'
                     AND completed_at IS NULL
             """)
             count = cursor.fetchone()[0]
+            self.conn.commit()
             return count
 
     def add(self, task, lease_timeout, ttl=3):
@@ -115,26 +123,33 @@ class TaskQueue:
 
         with self.conn.cursor() as cursor:
             # store the task + metadata and put task-id into the task queue
-            cursor.execute(
-                f"""INSERT INTO task_queue (
-                        id,
-                        queue_name,
-                        task,
-                        ttl,
-                        lease_timeout
-                    )
-                    VALUES (%s, %s, %s, %s, %s)
-                """, (id_, self._queue_name, task, ttl, lease_timeout))
+            cursor.execute("""
+                INSERT INTO task_queue (
+                    id,
+                    queue_name,
+                    task,
+                    ttl,
+                    lease_timeout
+                )
+                VALUES (%s, %s, %s, %s, %s)
+            """, (id_, self._queue_name, task, ttl, lease_timeout))
             self.conn.commit()
 
     def get(self):
         """Get a task from the task queue (non-blocking).
 
-        This statement marks the next available task in the queue as "processing" and returns its ID and task details. The query uses a FOR UPDATE SKIP LOCKED clause to lock the selected task so that other workers can't select the same task simultaneously.
+        This statement marks the next available task in the queue as
+        "processing" and returns its ID and task details. The query
+        uses a FOR UPDATE SKIP LOCKED clause to lock the selected
+        task so that other workers can't select the same task simultaneously.
 
-        After executing the query, the method fetches the result using cur.fetchone(). If no task is found, the method returns None, None. Otherwise, it returns the task and its ID.
+        After executing the query, the method fetches the result using
+        cur.fetchone(). If no task is found, the method returns None, None.
+        Otherwise, it returns the task and its ID.
 
-        Note that this method is non-blocking, which means it returns immediately even if there is no task available in the queue.. In order to mark that task as done, you have
+        Note that this method is non-blocking, which means it returns
+        immediately even if there is no task available in the queue..
+        In order to mark that task as done, you have
         to use:
 
             >>> task, task_id = taskqueue.get()
@@ -160,22 +175,23 @@ class TaskQueue:
 
         with conn.cursor() as cur:
 
-            cur.execute(
-                """ UPDATE task_queue
-                    SET processing = true,
-                        deadline = NOW() + CAST(lease_timeout || ' seconds' AS INTERVAL)
-                    WHERE id = (
-                        SELECT id
-                        FROM task_queue
-                        WHERE completed_at IS NULL 
-                            AND processing = false
-                            AND queue_name = %s
-                            AND ttl > 0
-                        ORDER BY created_at
-                        FOR UPDATE SKIP LOCKED
-                        LIMIT 1
-                    )
-                    RETURNING id, task;""",
+            cur.execute(f"""
+                UPDATE {self._table_name}
+                SET processing = true,
+                    deadline =
+                        NOW() + CAST(lease_timeout || ' seconds' AS INTERVAL)
+                WHERE id = (
+                    SELECT id
+                    FROM {self._table_name}
+                    WHERE completed_at IS NULL
+                        AND processing = false
+                        AND queue_name = %s
+                        AND ttl > 0
+                    ORDER BY created_at
+                    FOR UPDATE SKIP LOCKED
+                    LIMIT 1
+                )
+                RETURNING id, task;""",
                 (self._queue_name,),
             )
 
@@ -187,7 +203,6 @@ class TaskQueue:
             logger.info(f'Got task with id {task_id}')
             conn.commit()
             return task, task_id
-
 
     def complete(self, task_id):
         """Mark a task as completed.
@@ -209,13 +224,14 @@ class TaskQueue:
         conn = self.conn
         with conn.cursor() as cur:
 
-            cur.execute(
-                """ UPDATE task_queue
-                    SET completed_at = NOW(),
-                        processing = false
-                    WHERE id = %s""",
+            cur.execute(f"""
+                UPDATE {self._table_name}
+                SET completed_at = NOW(),
+                    processing = false
+                WHERE id = %s""",
                 (task_id,),
             )
+            conn.commit()
 
     def is_empty(self):
         """Check if the task queue is empty.
@@ -239,7 +255,7 @@ class TaskQueue:
         and checks if their deadline expired. If not we assume the
         worker died. We decrease the TTL and if TTL is still > 0 we
         reschedule the task into the task queue or, if the TTL is
-        exhausted, we mark the task as completed by setting 
+        exhausted, we mark the task as completed by setting
         `completed_at` column with current timestamp.
 
         Note: lease check is only performed against the tasks in
@@ -248,24 +264,21 @@ class TaskQueue:
         """
         # goes through all the tasks that are marked as processing
         # and check the ones with expired timeout
-        breakpoint()
         with self.conn.cursor() as cur:
-            cur.execute("""
+            cur.execute(f"""
                 SELECT id
-                FROM task_queue
-                WHERE completed_at IS NULL 
+                FROM {self._table_name}
+                WHERE completed_at IS NULL
                     AND processing = true
                     AND queue_name = %s
                     AND deadline < NOW()
-                ORDER BY created_at
+                ORDER BY created_at;
             """, (self._queue_name,))
-            
             expired_tasks = cur.fetchall()
+            self.conn.commit()
             logger.info(f"Expired tasks {expired_tasks}")
-
         for row in expired_tasks:
             task_id = row[0]
-            logger.info(f"Got expired task with type(id) {type(task_id)}")
             logger.info(f"Got expired task with id {task_id}")
             with self.conn.cursor() as cur:
                 cur.execute(f"""
@@ -274,13 +287,12 @@ class TaskQueue:
                         processing = false,
                         deadline = NULL
                     WHERE id = (
-                        SELECT id 
+                        SELECT id
                         FROM {self._table_name}
-                        WHERE completed_at IS NULL 
+                        WHERE completed_at IS NULL
                             AND processing = true
                             AND queue_name = %s
                             AND id = %s
-                        ORDER BY created_at
                         FOR UPDATE SKIP LOCKED
                         LIMIT 1
                     )
@@ -290,6 +302,7 @@ class TaskQueue:
                 updated_row = cur.fetchone()
 
             if updated_row is None:
+                # TODO add a test
                 # race condition! between the time we got `key` from the
                 # set of tasks (this outer loop) and the time we tried
                 # to get that task from the queue, it has been completed
@@ -298,34 +311,19 @@ class TaskQueue:
                 logger.info(f"Task {task_id} was marked completed while we "
                             "checked for expired leases, nothing to do.")
                 continue
-            
+
             wrapped_task, ttl = updated_row
             task = self._serialize(wrapped_task['task'])
 
-
             if ttl <= 0:
                 logger.error(f'Job {task} with id {task_id} '
-                                'failed too many times, marking it as completed.')
+                             'failed too many times, marking it as completed.')
                 # # here committing to release the previous update lock
-                with self.conn.cursor() as cur:
-                    cur.execute(f"""
-                        UPDATE {self._table_name}
-                        SET completed_at = NOW(),
-                            processing = false
-                        WHERE id = (
-                            SELECT id 
-                            FROM {self._table_name}
-                            WHERE completed_at IS NULL 
-                                AND id = %s
-                            FOR UPDATE SKIP LOCKED
-                            LIMIT 1
-                        )
-                        RETURNING id;
-                    """, (task_id,))
+                self.conn.commit()
+                self.complete(task_id)
 
                 if self.ttl_zero_callback:
                     self.ttl_zero_callback(task_id, task)
-
             self.conn.commit()
 
     def _serialize(self, task):
@@ -336,13 +334,94 @@ class TaskQueue:
         task = json.loads(blob)
         return task
 
+
+    def reschedule(self, task_id):
+        """Move a task back from the processing- to the task queue.
+
+        Workers can use this method to "drop" a work unit in case of
+        eviction.
+
+        This function does not modify the TTL.
+
+        Parameters
+        ----------
+        task_id : str
+            the task ID
+
+        Raises
+        ------
+        ValueError :
+            Task is not being processed, and cannot be re-scheduled
+
+        """
+
+        if not isinstance(task_id, UUID):
+            raise ValueError("task_id must be a UUID")
+        logger.info(f'Rescheduling task {task_id}..')
+        conn = self.conn
+        with conn.cursor() as cur:
+
+            cur.execute(f"""
+                UPDATE {self._table_name}
+                SET processing = false,
+                    deadline = NULL
+                WHERE id = (
+                    SELECT id
+                    FROM {self._table_name}
+                    WHERE processing = true
+                        AND id = %s
+                    FOR UPDATE SKIP LOCKED
+                )
+                RETURNING id;""",
+                (task_id,),
+            )
+
+            found = cur.fetchone()
+            conn.commit()
+            if found is None:
+                raise ValueError(f'Task {task_id} does not exist.')
+
+
     def _reset(self):
-        """Delete all tasks in the DB prefixed with our queue name.
+        """Delete all tasks in the DB with our queue name.
 
         """
         with self.conn.cursor() as cursor:
             cursor.execute(
                 f"DELETE FROM {self._table_name} \
                    WHERE queue_name = %s ", (self._queue_name,),)
-            
+
             self.conn.commit()
+
+
+    def __iter__(self):
+        """Iterate over tasks and mark them as complete.
+
+        This allows to easily iterate over the tasks to process them:
+
+            >>> for task in task_queue:
+                    execute_task(task)
+
+        it takes care of marking the tasks as done once they are processed
+        and checking the emptiness of the queue before leaving.
+
+        Notice that this iterator can wait for a long time waiting for work
+        units to appear, depending on the value set as lease_timeout.
+
+        Yields
+        -------
+        (any, str) :
+            A tuple containing the task content and its id
+
+        """
+        while True:
+            task, id_ = self.get()
+            if task is not None:
+                yield task, id_
+                self.complete(id_)
+            if self.is_empty():
+                logger.debug(
+                    f'{self._queue_name} is empty. '
+                    'Nothing to process anymore...'
+                )
+                break
--- tests/test_task_queue.py
+++ tests/test_task_queue.py
@@ -9,7 +9,7 @@ import pytest
 from postgrestq import TaskQueue
 
 
-POSTGRES_DSN = os.environ.get('POSTGRES_DSN', 'postgresql://postgres:mysecretpassword@localhost:5432/postgres') #noqa
+POSTGRES_DSN = os.environ.get('POSTGRES_DSN', 'postgresql://postgres:password@localhost:5432/postgres') #noqa
 LEASE_TIMEOUT = 0.1
 
 logger = logging.getLogger(__name__)
@@ -83,7 +83,7 @@ def test_complete(taskqueue):
 
 
 def test_expired(taskqueue):
-    taskqueue.add('foo', LEASE_TIMEOUT, 1)
+    taskqueue.add('foo', LEASE_TIMEOUT, ttl=1)
     taskqueue.get()
     assert not taskqueue.is_empty()
     time.sleep(LEASE_TIMEOUT + 0.1)
@@ -99,174 +99,174 @@ def test_expired(taskqueue):
     assert tend - tstart > LEASE_TIMEOUT
 
 
-#
-# def test_ttl(taskqueue, caplog):
-#     taskqueue.add('foo', LEASE_TIMEOUT, ttl=3)
 
-#     # start a task and let it expire...
-#     taskqueue.get()
-#     time.sleep(LEASE_TIMEOUT + 0.1)
-#     # check and put it back into task queue
-#     assert not taskqueue.is_empty()
+def test_ttl(taskqueue, caplog):
+    taskqueue.add('foo', LEASE_TIMEOUT, ttl=3)
 
-#     # second attempt...
-#     taskqueue.get()
-#     time.sleep(LEASE_TIMEOUT + 0.1)
-#     assert not taskqueue.is_empty()
+    # start a task and let it expire...
+    taskqueue.get()
+    time.sleep(LEASE_TIMEOUT + 0.1)
+    # check and put it back into task queue
+    assert not taskqueue.is_empty()
 
-#     # third attempt... *boom*
-#     taskqueue.get()
-#     time.sleep(LEASE_TIMEOUT + 0.1)
-#     caplog.clear()
-#     assert taskqueue.is_empty()
-#     assert "failed too many times" in caplog.text
+    # second attempt...
+    taskqueue.get()
+    time.sleep(LEASE_TIMEOUT + 0.1)
+    assert not taskqueue.is_empty()
 
-
-#
-# def test_callback(taskqueue):
-
-#     mock_cb = mock.Mock()
-#     taskqueue.ttl_zero_callback = mock_cb
-
-#     taskqueue.add('foo', LEASE_TIMEOUT, ttl=3)
-
-#     # start a task and let it expire...
-#     taskqueue.get()
-#     time.sleep(LEASE_TIMEOUT + 0.1)
-#     # check and put it back into task queue
-#     assert not taskqueue.is_empty()
-#     assert not mock_cb.called
-
-#     # second attempt...
-#     taskqueue.get()
-#     time.sleep(LEASE_TIMEOUT + 0.1)
-#     assert not taskqueue.is_empty()
-#     assert not mock_cb.called
-
-#     # third attempt... *boom*
-#     taskqueue.get()
-#     time.sleep(LEASE_TIMEOUT + 0.1)
-#     assert taskqueue.is_empty()
-#     assert mock_cb.called
+    # third attempt... *boom*
+    taskqueue.get()
+    time.sleep(LEASE_TIMEOUT + 0.1)
+    caplog.clear()
+    assert taskqueue.is_empty()
+    assert "failed too many times" in caplog.text
 
 
-#
-# def test_reschedule(taskqueue):
-#     taskqueue.add('foo', LEASE_TIMEOUT)
-#     _, id_ = taskqueue.get()
-#     # task queue should be empty as 'foo' is in the processing queue
-#     assert taskqueue.get() == (None, None)
 
-#     taskqueue.reschedule(id_)
-#     task, _ = taskqueue.get()
-#     assert task == 'foo'
+def test_callback(taskqueue):
 
+    mock_cb = mock.Mock()
+    taskqueue.ttl_zero_callback = mock_cb
 
-#
-# def test_reschedule_error(taskqueue):
-#     with pytest.raises(ValueError):
-#         taskqueue.reschedule('bar')
+    taskqueue.add('foo', LEASE_TIMEOUT, ttl=3)
 
+    # start a task and let it expire...
+    taskqueue.get()
+    time.sleep(LEASE_TIMEOUT + 0.1)
+    # check and put it back into task queue
+    assert not taskqueue.is_empty()
+    assert not mock_cb.called
 
-#
-# def test_full(taskqueue):
-#     TASKS = ['FOO', 'BAR', 'BAZ']
-#     for t in TASKS:
-#         taskqueue.add(t, LEASE_TIMEOUT)
+    # second attempt...
+    taskqueue.get()
+    time.sleep(LEASE_TIMEOUT + 0.1)
+    assert not taskqueue.is_empty()
+    assert not mock_cb.called
 
-#     counter = 0
-#     while True:
-#         task, task_id = taskqueue.get()
-#         if task is not None:
-#             taskqueue.complete(task_id)
-#             counter += 1
-#         if taskqueue.is_empty():
-#             break
-
-#     assert counter == len(TASKS)
+    # third attempt... *boom*
+    taskqueue.get()
+    time.sleep(LEASE_TIMEOUT + 0.1)
+    assert taskqueue.is_empty()
+    assert mock_cb.called
 
 
-#
-# def test_complete_rescheduled_task(taskqueue):
-#     TASK_CONTENT = 'sloth'
-#     taskqueue.add(TASK_CONTENT, LEASE_TIMEOUT, ttl=3)
 
-#     # start a task and let it expire...
-#     _, task_id = taskqueue.get()
-#     time.sleep(LEASE_TIMEOUT + 0.1)
+def test_reschedule(taskqueue):
+    taskqueue.add('foo', LEASE_TIMEOUT)
+    _, id_ = taskqueue.get()
+    # task queue should be empty as 'foo' is in the processing queue
+    assert taskqueue.get() == (None, None)
 
-#     # check and put it back into task queue
-#     assert not taskqueue.is_empty()
-
-#     # now the task is completed, although it took a long time...
-#     taskqueue.complete(task_id)
-
-#     # it is NOT in the taskqueue, because it was finished
-#     assert taskqueue.is_empty()
+    taskqueue.reschedule(id_)
+    task, _ = taskqueue.get()
+    assert task == 'foo'
 
 
-#
-# def test_tolerate_double_completion(taskqueue):
-#     TASK_CONTENT = 'sloth'
-#     taskqueue.add(TASK_CONTENT, LEASE_TIMEOUT, ttl=3)
 
-#     # start a task and let it expire...
-#     task, task_id = taskqueue.get()
-#     time.sleep(LEASE_TIMEOUT + 0.1)
+def test_reschedule_error(taskqueue):
+    with pytest.raises(ValueError):
+        taskqueue.reschedule('bar')
 
-#     # check and put it back into task queue
-#     assert not taskqueue.is_empty()
 
-#     # get it again
-#     _, task_redo_id = taskqueue.get()
-#     assert task_redo_id == task_id
 
-#     # now the task is completed, although it took a long time...
-#     taskqueue.complete(task_id)
+def test_full(taskqueue):
+    TASKS = ['FOO', 'BAR', 'BAZ']
+    for t in TASKS:
+        taskqueue.add(t, LEASE_TIMEOUT)
 
-#     # but the other worker doesn't know and keep processing, until...
-#     taskqueue.complete(task_redo_id)
+    counter = 0
+    while True:
+        task, task_id = taskqueue.get()
+        if task is not None:
+            taskqueue.complete(task_id)
+            counter += 1
+        if taskqueue.is_empty():
+            break
 
-#     # no crashes, the double completion is fine and queues are empty
-#     assert taskqueue.is_empty()
+    assert counter == len(TASKS)
+
+
+
+def test_complete_rescheduled_task(taskqueue):
+    TASK_CONTENT = 'sloth'
+    taskqueue.add(TASK_CONTENT, LEASE_TIMEOUT, ttl=3)
+
+    # start a task and let it expire...
+    _, task_id = taskqueue.get()
+    time.sleep(LEASE_TIMEOUT + 0.1)
+
+    # check and put it back into task queue
+    assert not taskqueue.is_empty()
+
+    # now the task is completed, although it took a long time...
+    taskqueue.complete(task_id)
+
+    # it is NOT in the taskqueue, because it was finished
+    assert taskqueue.is_empty()
 
 
 #
-# def test_task_queue_len(taskqueue):
+def test_tolerate_double_completion(taskqueue):
+    TASK_CONTENT = 'sloth'
+    taskqueue.add(TASK_CONTENT, LEASE_TIMEOUT, ttl=3)
 
-#     # empty queue
-#     assert len(taskqueue) == 0
+    # start a task and let it expire...
+    task, task_id = taskqueue.get()
+    time.sleep(LEASE_TIMEOUT + 0.1)
 
-#     # insert two tasks
-#     TASKS = ['foo', 'bar']
-#     for task in TASKS:
-#         taskqueue.add(task, LEASE_TIMEOUT)
-#     assert len(taskqueue) == len(TASKS)
+    # check and put it back into task queue
+    assert not taskqueue.is_empty()
 
-#     # removing getting the tasks w/o completing them
-#     ids = []
-#     for task in TASKS:
-#         ids.append(taskqueue.get()[1])
-#     assert len(taskqueue) == len(TASKS)
+    # get it again
+    _, task_redo_id = taskqueue.get()
+    assert task_redo_id == task_id
 
-#     for id_ in ids:
-#         taskqueue.complete(id_)
-#     assert len(taskqueue) == 0
+    # now the task is completed, although it took a long time...
+    taskqueue.complete(task_id)
 
+    # but the other worker doesn't know and keep processing, until...
+    taskqueue.complete(task_redo_id)
 
-#
-# def test_iterator(taskqueue):
-#     taskqueue.add('bla', LEASE_TIMEOUT, ttl=3)
-#     taskqueue.add('blip', LEASE_TIMEOUT, ttl=3)
-
-#     found_tasks = []
-#     for task, id in taskqueue:
-#         found_tasks.append(task)
-#     assert found_tasks == ['bla', 'blip']
+    # no crashes, the double completion is fine and queues are empty
+    assert taskqueue.is_empty()
 
 
-#
-# def test_exired_leases_race(taskqueue, monkeypatch, caplog):
+
+def test_task_queue_len(taskqueue):
+
+    # empty queue
+    assert len(taskqueue) == 0
+
+    # insert two tasks
+    TASKS = ['foo', 'bar']
+    for task in TASKS:
+        taskqueue.add(task, LEASE_TIMEOUT)
+    assert len(taskqueue) == len(TASKS)
+
+    # removing getting the tasks w/o completing them
+    ids = []
+    for task in TASKS:
+        ids.append(taskqueue.get()[1])
+    assert len(taskqueue) == len(TASKS)
+
+    for id_ in ids:
+        taskqueue.complete(id_)
+    assert len(taskqueue) == 0
+
+
+
+def test_iterator(taskqueue):
+    taskqueue.add('bla', LEASE_TIMEOUT, ttl=3)
+    taskqueue.add('blip', LEASE_TIMEOUT, ttl=3)
+
+    found_tasks = []
+    for task, id in taskqueue:
+        found_tasks.append(task)
+    assert found_tasks == ['bla', 'blip']
+
+
+
+# def test_expired_leases_race(taskqueue, monkeypatch, caplog):
 #     # save the original conn.get so we can use it inside the mock
 #     get_orig = taskqueue.conn.get
 
@@ -285,71 +285,21 @@ def test_expired(taskqueue):
 #     # move task to processing queue
 #     taskqueue.get()
 
-#     monkeypatch.setattr(taskqueue.conn, 'get', mock_get)
-#     caplog.set_level(logging.INFO)
-#     taskqueue._check_expired_leases()
-#     assert "marked completed while we checked for" in caplog.text
+    monkeypatch.setattr(taskqueue.conn, 'get', mock_get)
+    caplog.set_level(logging.INFO)
+    taskqueue._check_expired_leases()
+    assert "marked completed while we checked for" in caplog.text
 
 
-#
-# def test_lease_timout_is_none(taskqueue):
-#     with pytest.raises(TypeError):
-#         taskqueue.add('bla', lease_timeout=None)
+
+def test_lease_timeout_is_none(taskqueue):
+    with pytest.raises(TypeError):
+        taskqueue.add('bla', lease_timeout=None)
 
 
-#
-# def test_lease_timout_is_not_float_or_int(taskqueue):
-#     # funny thing, a boolean can be converted to float (i.e. 0.0 and
-#     # 1.0) without causing an error. so be it
-#     with pytest.raises(ValueError):
-#         taskqueue.add('bla', lease_timeout="foo")
 
-
-#
-# def test_acquire_lock_success(taskqueue):
-#     # worker successfuly acquires lock
-#     assert taskqueue._acquire_lock(taskqueue.get_lock)
-
-
-#
-# def test_acquire_lock_fail(taskqueue):
-#     # mimick lock obtained by other worker
-#     taskqueue._acquire_lock(taskqueue.get_lock)
-
-#     # now current worker should not be able to get the lock
-#     taskqueue.client_id = 1234
-#     assert not taskqueue._acquire_lock(taskqueue.get_lock)
-
-
-#
-# def test_lock_automatically_releases(taskqueue):
-#     # mimick lock obtained by other worker
-#     taskqueue.lock_expiry = 2
-#     taskqueue._acquire_lock(taskqueue.get_lock)
-
-#     # now current worker should get lock after waiting for lock timeout
-#     taskqueue.client_id = 1234
-#     time.sleep(taskqueue.lock_expiry)
-#     assert taskqueue._acquire_lock(taskqueue.get_lock)
-
-
-#
-# def test_release_lock_it_owns(taskqueue):
-#     # acquire lock
-#     taskqueue._acquire_lock(taskqueue.get_lock)
-
-#     # release its own lock
-#     taskqueue._release_lock(taskqueue.get_lock)
-#     assert taskqueue.conn.get(taskqueue.get_lock) is None
-
-
-#
-# def test_do_not_release_lock_owned_by_other(taskqueue):
-#     # mimick lock obtained by other worker
-#     client_id = taskqueue.client_id
-#     taskqueue._acquire_lock(taskqueue.get_lock)
-
-#     # current worker should not release the lock owned by other worker
-#     taskqueue.client_id = 1234
-#     taskqueue._release_lock(taskqueue.get_lock)
-#     assert taskqueue.conn.get(taskqueue.get_lock).decode() == client_id
+def test_lease_timeout_is_not_float_or_int(taskqueue):
+    # funny thing, a boolean can be converted to float (i.e. 0.0 and
+    # 1.0) without causing an error. so be it
+    with pytest.raises(ValueError):
+        taskqueue.add('bla', lease_timeout="foo")
